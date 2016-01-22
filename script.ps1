d6bf4ca8dfb1712375e8f0bfab4be593954b9d5d
aglesaint <aglesaint@gmail.com>
2016-01-22 14:44:06 +0100

    maj de la commande ADD-XXXSite

--- script.ps1
+++ script.ps1
@@ -1,3 +1,7 @@
 import-module .\sharepoint.cephinet.info.dll
 #Ligne à mettre systematiquement (charge les parametres du site)
 [xml]$param = Get-Content -Path sharepoint.cephinet.info.xml
+# Ajout de site
+# Add-XXXSite <site collection> <uri> <descriptif> <administrateur> $param
+# Exemple :
+Add-XXXSite "http://w2k8serverr2b/my/personal/aglesaint" "AutoCreateDemo" "Test de creation automatique de site" "CEPHINET\Administrateur" $param
